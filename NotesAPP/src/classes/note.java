3981129a6b5b05e610eb31b372c9731e96a0a64f
Fletcher18b <oiramgrillol.2@gmail.com>
2023-06-24 21:19:46 -0600

    Advances 6/24
    Added the GUI for Main menu, new note and comfirm

---
+++ NotesAPP/src/classes/note.java
@@ -0,0 +1,29 @@
+package classes;
+
+
+public class note {
+    
+    
+    public void new_line() {
+        
+    }
+    
+    public String getCurrent_line() {
+        return current_line;
+    }
+
+    public void setCurrent_line(String current_line) {
+        this.current_line = current_line;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    String current_line;
+    String title;
+    
+}
